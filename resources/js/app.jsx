1c751bc330d25cc14ba3f68112379d7bfdfc372c
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2022-12-14 00:46:17 +0000

    Added react

---
+++ resources/js/app.jsx
@@ -0,0 +1,10 @@
+import '../css/app.css';
+import './bootstrap';
+
+import ReactDOM from 'react-dom/client';
+import { Home } from '../components/Home';
+import '../css/app.css';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Home />
+);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
+        react(),
     ],
 });
